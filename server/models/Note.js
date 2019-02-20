bf25659d8a0275d0ce42f4f7b9f931a6c66d2d6b
Nils Lundgren <n.lunpat@gmail.com>
2019-02-20 19:31:17 +0100

    controllers created

--- server/models/Note.js
+++ server/models/Note.js
@@ -5,7 +5,9 @@ let NoteSchema = new mongoose.Schema(
         title: String, 
         description: String,
         feture_img: String,
-        star: Boolean,
+        star: { type: Boolean,
+                default: false
+        },
         author: {
             type: mongoose.Schema.Types.ObjectId,
             ref: 'User'
@@ -21,6 +23,27 @@ let NoteSchema = new mongoose.Schema(
         ]
     }
 );
-ArticleSchema.methods.star = function() {
-    this.
+NoteSchema.methods.star = function() {
+    if (this.star === false) {
+        this.star = true
+    } else {
+        this.star = false
+    }
+    return this.save()
 }
+NoteSchema.methods.comment = function(c) {
+    this.comments.push(c)
+    return this.save()
+}
+NoteSchema.methods.addAuthor = function (author_id) {
+    this.author = author_id 
+    return this.save()
+}
+NoteSchema.methods.getUserNote = funtion(_id) {
+    ArticleSchema.find({'author': _id}).then((note) => {
+        return note
+    })
+}
+
+module.exports = mongoose.model('Note', NoteSchema)
+
