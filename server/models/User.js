7c9662ae44ff4133d0da801040a84335815cf919
Nils Lundgren <n.lunpat@gmail.com>
2019-02-22 21:51:30 +0100

    server started found bug

--- server/models/User.js
+++ server/models/User.js
@@ -10,4 +10,4 @@ let UserSchema = new mongoose.Schema(
         provider_pic: String
     }
 )
-module.exports = mongoose.model('User', UserSchama)
+module.exports = mongoose.model('User', UserSchema)
